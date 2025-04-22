2e842f37b5b994063964fe305d1e4bbccd0728c0
realdemopan <147283324+realdemopan@users.noreply.github.com>
2025-04-22 00:07:59 -0400

    Create service-worker.js

---
+++ crashout/service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'crashout-cache-v1';
+const FILES_TO_CACHE = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
